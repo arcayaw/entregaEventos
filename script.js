6ba003ab0bf6be5ddaf00aa8ab52499b88a07a31
Willian Arcaya <willianarcaya@Willians-MacBook-Pro.local>
2022-03-01 16:14:32 -0300

    ready

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-// //codigo html de referencia
-// <div id="contenedor"></div>;
-
-// //codigo JS
-// let contenedor = document.getElementById("contenedor");
-
-// //cambio el codigo HTML interno
-// contenedor.innerHTML = "<h2>Hola mundo</h2><p>Texto de prueba</p>";
-
-// //resultado en el dom
-// <div id="contenedor">
-//   <h2>Hola mundo</h2>
-//   <p>Texto de prueba</p>
-// </div>;
-// /* Comment */
-
-// //crear nodo de tipo Elemento, etiqueta p
-// let parrafo = document.getElementById("parrafo1");
-// //eliminando el propio elemento.
-// parrafo.remove();
-
-// let paises = document.getElementsByClassName("paises");
-// //eliminando el primer elemento de CLASE PAISES.
-// paises[0].remove();
-
-// /* Comment */
-
 //creo una clase
 class Box {
   //creo un constructor
@@ -37,9 +10,9 @@ class Box {
   }
 }
 //creo tres boxes con el constructor
-const box1 = new Box(1, "uno", 1500, "otra", 10);
-const box2 = new Box(2, "dos", 2500, "otra", 20);
-const box3 = new Box(3, "tres", 3500, "otra", 30);
+const box1 = new Box(1, "Box Kids", 1500, "Cumpleaños", 10);
+const box2 = new Box(2, "Box Spa", 2500, "Relax", 20);
+const box3 = new Box(3, "Box fiesta", 3500, "Cumpleaños", 30);
 
 //creo un array con las cajas creadas
 const arrayCajas = [box1, box2, box3];
@@ -51,15 +24,41 @@ let divCajas = document.getElementById("divCajas");
 //creo el alias cajaenArray, que es una forma de identificar con una palabra cada uno de los elementos de mi array, fuera del forEach no existe.
 //utilizamos id como identificador ya que es la unica propiedad que no se repite.
 //colocamos += para que muestre todos los elementos, si colocamos solo = muestra el ultimo.
+
+let formEnvio = document.getElementById("idForm");
+
+formEnvio.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let nombreForm = document.getElementById("nombreForm");
+  let apellidoForm = document.getElementById("apellidoForm");
+  let direccionForm = document.getElementById("direccionForm");
+  let phoneForm = document.getElementById("phoneForm");
+  let emailForm = document.getElementById("emailForm");
+
+  divEnvio.innerHTML = `
+    <div class="m-5">
+      <div>
+        <h3>Hola ${nombreForm.value} ${apellidoForm.value}</h3>
+      </div>
+      <div>
+        <p>Tus productos seran enviados a la direccion: ${direccionForm.value}</p>
+        <p>Te estaremos avisando al estatus del envio al correo ${emailForm.value} o al telefono ${phoneForm.value}</p>
+      </div>
+    
+    </div>
+
+`;
+});
+
 arrayCajas.forEach((cajaenArray) => {
   divCajas.innerHTML += `
-  
-  <div id= "${cajaenArray.id}" class"acaAgregamosEstilosCSS">
-    <p>nombre: ${cajaenArray.nombre} </p>
-    <p>precio: ${cajaenArray.precio}</p>
-    <p>categoria: ${cajaenArray.categoria}</p>
-    <p>stock: ${cajaenArray.stock}</p>
-  </div>
-  <hr></hr>
-  `;
+  <div class="card m-3" style="width: 18rem;">
+    <div class="card-body ">
+      <h5 class="card-title">Nombre: ${cajaenArray.nombre} </h5>
+      <p class="card-text">Precio: ${cajaenArray.precio}</p>
+      <p class="card-text">Categoria: ${cajaenArray.categoria}</p>
+      <p class="card-text">Stock: ${cajaenArray.stock}</p>
+      <a href="#" class="btn btn-primary text-center">Comprar</a>
+    </div>
+  </div>`;
 });
